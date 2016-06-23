bd55ca05f91a106be69eda6ef207a3315837d513
DDiimmkkaass <ddiimmkkaass@gmail.com>
2016-06-23 15:16:17 +0300

    remove price duplication on ingredient form, fix #26

--- resources/themes/admin/views/ingredient/tabs/general.blade.php
+++ resources/themes/admin/views/ingredient/tabs/general.blade.php
@@ -27,13 +27,3 @@
         {!! $errors->first('category_id', '<p class="help-block error">:message</p>') !!}
     </div>
 </div>
-
-<div class="form-group required @if ($errors->has('price')) has-error @endif">
-    {!! Form::label('price', trans('labels.price'), ['class' => 'control-label col-xs-12 col-sm-3 col-md-2']) !!}
-
-    <div class="col-xs-12 col-sm-2 col-md-1">
-        {!! Form::text('price', $model->price ?: 0, ['class' => 'form-control input-sm', 'aria-hidden' => 'true', 'required' => true]) !!}
-
-        {!! $errors->first('price', '<p class="help-block error">:message</p>') !!}
-    </div>
-</div>
