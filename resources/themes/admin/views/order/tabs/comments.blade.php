103629348466ede574a2d5d7b40b676cf39f91a1
DDiimmkkaass <ddiimmkkaass@gmail.com>
2016-08-28 18:00:30 +0300

    order comment add form only for exists orders

--- resources/themes/admin/views/order/tabs/comments.blade.php
+++ resources/themes/admin/views/order/tabs/comments.blade.php
@@ -1,25 +1,28 @@
-<div id="order_comments_block" class="box box-primary order-comments-block">
-    <div class="box-header">
-        <h4 class="margin-top-10 margin-bottom-0">@lang('labels.comments')</h4>
-    </div>
-    <div class="box-body">
-        <div class="direct-chat-messages height-auto comments-block">
-            @each('order.partials.comment', $model->comments, 'comment')
+@if ($model->exists)
+    <div id="order_comments_block" class="box box-primary order-comments-block">
+        <div class="box-header">
+            <h4 class="margin-top-10 margin-bottom-0">@lang('labels.comments')</h4>
         </div>
-    </div>
-    <div class="box-footer">
-        <div data-action="{!! route('admin.order.comment.store') !!}" class="order-comment-form">
-            <div class="form-group">
-                <div class="col-sm-12">
-                    <div class="input-group">
-                        <input type="hidden" id="order_id" name="order_id" value="{!! $model->id !!}">
-                        <input type="text" id="order_comment" name="order_comment" placeholder="@lang('labels.type_a_comment')" class="form-control input-sm">
-                        <span class="input-group-btn">
+        <div class="box-body">
+            <div class="direct-chat-messages height-auto comments-block">
+                @each('order.partials.comment', $model->comments, 'comment')
+            </div>
+        </div>
+        <div class="box-footer">
+            <div data-action="{!! route('admin.order.comment.store') !!}" class="order-comment-form">
+                <div class="form-group">
+                    <div class="col-sm-12">
+                        <div class="input-group">
+                            <input type="hidden" id="order_id" name="order_id" value="{!! $model->id !!}">
+                            <input type="text" id="order_comment" name="order_comment"
+                                   placeholder="@lang('labels.type_a_comment')" class="form-control input-sm">
+                            <span class="input-group-btn">
                             <button type="button" class="btn btn-warning btn-flat btn-sm">@lang('labels.add')</button>
                         </span>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+@endif
