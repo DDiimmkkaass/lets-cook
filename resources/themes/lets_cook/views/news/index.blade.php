e568b78ae795e7ca0591d05196a4df39d0aaa24a
DDiimmkkaass <ddiimmkkaass@gmail.com>
2016-09-07 01:06:30 +0300

    blog page h1

--- resources/themes/lets_cook/views/news/index.blade.php
+++ resources/themes/lets_cook/views/news/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     <main class="main blog-list articles-list">
-        <h1 class="articles-list__title static-georgia-title" data-page="articles"><span>Статьи</span></h1>
+        <h1 class="articles-list__title static-georgia-title" data-page="articles"><span>Новости</span></h1>
 
         @if (count($list))
             @include('news.partials.filters')
