c3c73a1517d1a291a1aa32bdba259d1485db1be9
DDiimmkkaass <ddiimmkkaass@gmail.com>
2016-09-14 20:40:18 +0300

    fix order when main basket not selected

--- resources/themes/admin/views/order/datatables/baskets_list.blade.php
+++ resources/themes/admin/views/order/datatables/baskets_list.blade.php
@@ -1,8 +1,10 @@
-<div class="basket-row">
-    <div class="pull-left">{!! $model->main_basket->getName() !!}</div>
-    <div class="pull-right">{!! $model->main_basket->price !!}</div>
-    <div class="clearfix"></div>
-</div>
+@unless (empty($model->main_basket))
+    <div class="basket-row">
+        <div class="pull-left">{!! $model->main_basket->getName() !!}</div>
+        <div class="pull-right">{!! $model->main_basket->price !!}</div>
+        <div class="clearfix"></div>
+    </div>
+@endunless
 @foreach($model->additional_baskets as $basket)
     <div class="basket-row">
         <div class="pull-left">{!! $basket->getName() !!}</div>
