61695e499d68e37cdc9ae053974543b05bf374af
DDiimmkkaass <ddiimmkkaass@gmail.com>
2016-09-29 22:51:48 +0300

    fix weekly menus widget for home

--- app/Widgets/WeeklyMenu/WeeklyMenuWidget.php
+++ app/Widgets/WeeklyMenu/WeeklyMenuWidget.php
@@ -9,6 +9,7 @@
 namespace App\Widgets\WeeklyMenu;
 
 use App\Models\WeeklyMenu;
+use DB;
 use Pingpong\Widget\Widget;
 
 /**
@@ -30,24 +31,47 @@ class WeeklyMenuWidget extends Widget
      */
     public function index($template = '')
     {
-        $menu = WeeklyMenu::with('baskets', 'baskets.recipes')->current()->first();
-        if ($menu && $menu->baskets->count()) {
-            $menu = $menu->baskets->random();
-        } else {
-            $menu = false;
-        }
-
-        $next_menu = WeeklyMenu::with('baskets', 'baskets.recipes')->next()->first();
-        if ($next_menu && $next_menu->baskets->count()) {
-            $next_menu = $next_menu->baskets->random();
-        } else {
-            $next_menu = false;
+        $active_week = active_week_menu_week();
+        
+        $menus = WeeklyMenu::joinWeeklyMenuBaskets()->joinBasketRecipes()
+            ->with('baskets', 'baskets.recipes')
+            ->active()
+            ->whereExists(
+                function ($query) {
+                    $query->select(DB::raw(1))
+                        ->from('basket_recipes')
+                        ->whereRaw('basket_recipes.weekly_menu_basket_id = weekly_menu_baskets.id')
+                        ->whereRaw('weekly_menu_baskets.weekly_menu_id = weekly_menus.id');
+                }
+            )
+            ->select('weekly_menus.*')
+            ->groupBy('weekly_menus.id')
+            ->take(2)
+            ->get();
+        
+        $menu = false;
+        $next_menu = false;
+        
+        foreach ($menus as $_menu) {
+            if ($_menu->week == $active_week->weekOfYear) {
+                if ($_menu->baskets->count()) {
+                    $menu = $_menu->baskets->random();
+                } else {
+                    $menu = false;
+                }
+            } else {
+                if ($_menu->baskets->count()) {
+                    $next_menu = $_menu->baskets->random();
+                } else {
+                    $next_menu = false;
+                }
+            }
         }
         
         if (view()->exists('widgets.weekly_menu.templates.'.$template.'.index')) {
             $this->template = $template;
         }
-    
+        
         return view('widgets.weekly_menu.templates.'.$this->template.'.index')
             ->with('menu', $menu)
             ->with('next_menu', $next_menu)
