04ca7fbd930cc4e77b9256d45f04a03dba8d4826
DDiimmkkaass <ddiimmkkaass@gmail.com>
2016-10-04 15:20:09 +0300

    format deliverys report

--- resources/themes/admin/views/packaging/download/deliveries.blade.php
+++ resources/themes/admin/views/packaging/download/deliveries.blade.php
@@ -1,4 +1,4 @@
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 
 @if (count($list))
     <div class="box-body table-responsive no-padding">
@@ -7,7 +7,9 @@
             @php($total = 0)
             @foreach($list as $day => $orders)
                 <tr style="height: 25px;">
-                    <th style="background-color: #3a9a18; height: 20px; vertical-align: bottom" colspan="8">{!! $day !!} ({!! day_of_week($day, 'd-m-Y') !!})</th>
+                    <th style="background-color: #3a9a18; height: 20px; vertical-align: bottom" colspan="8">{!! $day !!}
+                        ({!! day_of_week($day, 'd-m-Y') !!})
+                    </th>
                     <th style="background-color: #d6b505; vertical-align: bottom" colspan="2">
                         <div style="text-align: center;">@lang('labels.filled_by_the_user')</div>
                     </th>
@@ -47,8 +49,19 @@
 
                 @php($i = 1)
                 @foreach($orders as $order)
+                    @php($heigth = 15)
+                    @php($baskets = '<div>'.$order->main_basket->getName().'</div>')
+                    @if ($order->additional_baskets->count())
+                        @foreach($order->additional_baskets as $basket)
+                            @php($baskets .= '<br><div>'.$basket->getName().',</div>')
+                            @php($heigth += 15)
+                        @endforeach
+                    @endif
+
+                    @php($heigth = max($heigth, 30))
+
                     @php($background = $i % 2 == 0 ? ' background-color: #dddddd; ' : '')
-                    <tr style="height: 30px">
+                    <tr style="height: {!! $heigth !!}px">
                         <td style="text-align: center; {!! $background !!}">
                             {!! $i !!}
                         </td>
@@ -70,15 +83,10 @@
                             {!! $order->comment !!}
                         </td>
                         <td style="width: 30px; {!! $background !!}">
-                            {!! $order->main_basket->getName() !!}<br>
-                            @if ($order->additional_baskets->count())
-                                @foreach($order->additional_baskets as $basket)
-                                    {!! $basket->getName() !!},
-                                @endforeach
-                            @endif
+                            {!! $baskets !!}
                         </td>
                         <td style="width: 15px; text-align: center; {!! $background !!}">
-                            {!! $order->getPlacesCount() !!}
+                            {!! $order->getPlacesCount() !!} {!! $heigth !!}
                         </td>
                         <td style="width: 15px; text-align: center; color: #ff0000; {!! $background !!}">
                             @if ($order->paymentMethod('cash'))
@@ -94,12 +102,20 @@
                     @php($i++)
                 @endforeach
 
-                <tr><td colspan="10"></td></tr>
-                <tr><td colspan="10"></td></tr>
-                <tr><td colspan="10"></td></tr>
+                <tr>
+                    <td colspan="10"></td>
+                </tr>
+                <tr>
+                    <td colspan="10"></td>
+                </tr>
+                <tr>
+                    <td colspan="10"></td>
+                </tr>
             @endforeach
 
-            <tr><td colspan="10"></td></tr>
+            <tr>
+                <td colspan="10"></td>
+            </tr>
 
             <tr style="height: 20px">
                 <td colspan="6" style="text-align: right">@lang('labels.approve')</td>
@@ -112,9 +128,15 @@
                 <td colspan="2"></td>
             </tr>
 
-            <tr><td colspan="10"></td></tr>
-            <tr><td colspan="10"></td></tr>
-            <tr><td colspan="10"></td></tr>
+            <tr>
+                <td colspan="10"></td>
+            </tr>
+            <tr>
+                <td colspan="10"></td>
+            </tr>
+            <tr>
+                <td colspan="10"></td>
+            </tr>
 
             <tr style="height: 30px;">
                 <td colspan="6"></td>
