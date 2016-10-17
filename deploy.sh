bd8b5b137440b06b09f2fc81dc5deab6c0e87f6c
Terion <mail@terion.name>
2016-10-18 01:27:52 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-git reset --hard
-git pull origin master
+#git reset --hard
+#git pull origin master
 composer install --no-interaction --prefer-dist
 php artisan migrate --force
 ./node_modules/.bin/bower install
