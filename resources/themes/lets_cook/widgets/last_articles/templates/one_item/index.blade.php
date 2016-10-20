68952be961130969520345361cea1c2edd041abd
DDiimmkkaass <ddiimmkkaass@gmail.com>
2016-10-20 15:02:22 +0300

    fix last news & articles widgets

--- resources/themes/lets_cook/widgets/last_articles/templates/one_item/index.blade.php
+++ resources/themes/lets_cook/widgets/last_articles/templates/one_item/index.blade.php
@@ -1,8 +1,8 @@
 <div class="all-news-articles__item" data-order="2">
     @foreach($list as $item)
-        <h3 class="all-news-articles__title">{!! $item->title !!}</h3>
+        <h3 class="all-news-articles__title">{!! strip_tags($item->title) !!}</h3>
         <div class="all-news-articles__desc">
-            {!! str_limit($item->short_content, 95,'') !!}
+            {!! str_limit($item->getShortContent(), 95,'') !!}
         </div>
     @endforeach
     <a href="{!! route('articles.index') !!}" class="all-news-articles__link yellow-long-button">
