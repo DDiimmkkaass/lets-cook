ee3f9d07655e4fb99e644f6bac5d97756f6ca123
DDiimmkkaass <ddiimmkkaass@gmail.com>
2016-11-02 19:53:56 +0200

    contacts page

--- resources/themes/lets_cook/views/page/contacts.blade.php
+++ resources/themes/lets_cook/views/page/contacts.blade.php
@@ -8,40 +8,72 @@
         <div class="contacts-simple__info">
             <ul class="contacts-simple__info-wrapper">
                 <li class="contacts-simple__info-col">
-                    <div class="contacts-simple__info-row" data-contacts="address">
-                        {!! variable('address') !!}
-                    </div>
-                    <div class="contacts-simple__info-row" data-contacts="clock">
-                        <span class="contacts-simple__info-span">{!! variable('work_time') !!}</span>
-                        <span class="contacts-simple__info-span">{!! variable('work_days') !!}</span>
-                    </div>
+                    @php($address = variable('address'))
+                    @if ($address)
+                        <div class="contacts-simple__info-row" data-contacts="address">
+                            {!! $address !!}
+                        </div>
+                    @endif
+
+                    @php($work_time = variable('work_time'))
+                    @php($work_days = variable('work_days'))
+                    @if ($work_time || $work_days)
+                        <div class="contacts-simple__info-row" data-contacts="clock">
+                            @if ($work_time)
+                                <span class="contacts-simple__info-span">{!! $work_time !!}</span>
+                            @endif
+                            @if ($work_days)
+                                <span class="contacts-simple__info-span">{!! $work_days !!}</span>
+                            @endif
+                        </div>
+                    @endif
                 </li>
 
                 <li class="contacts-simple__info-col">
                     <div class="contacts-simple__info-row" data-contacts="phone">
-                        <a class="contacts-simple__info-link" href="tel:{!! preg_replace('/\D/', '', variable('moscow_phone')) !!}">
-                            {!! variable('moscow_phone') !!}
-                        </a>
-                        <a class="contacts-simple__info-link" href="tel:{!! preg_replace('/\D/', '', variable('phone_2')) !!}">
-                            {!! variable('phone_2') !!}
-                        </a>
-                        <a class="contacts-simple__info-link" href="tel:{!! preg_replace('/\D/', '', variable('phone_3')) !!}">
-                            {!! variable('phone_3') !!}
-                        </a>
+                        @php($moscow_phone = variable('moscow_phone'))
+                        @if ($moscow_phone)
+                            <a class="contacts-simple__info-link"
+                               href="tel:{!! preg_replace('/\D/', '', $moscow_phone) !!}">
+                                {!! $moscow_phone !!}
+                            </a>
+                        @endif
+
+                        @php($phone_2 = variable('phone_2'))
+                        @if ($phone_2)
+                            <a class="contacts-simple__info-link"
+                               href="tel:{!! preg_replace('/\D/', '', $phone_2) !!}">
+                                {!! $phone_2 !!}
+                            </a>
+                        @endif
+
+                        @php($phone_3 = variable('phone_3'))
+                        @if ($phone_3)
+                            <a class="contacts-simple__info-link"
+                               href="tel:{!! preg_replace('/\D/', '', $phone_3) !!}">
+                                {!! $phone_3 !!}
+                            </a>
+                        @endif
                     </div>
                 </li>
 
                 <li class="contacts-simple__info-col">
-                    <div class="contacts-simple__info-row" data-contacts="mail">
-                        <a class="contacts-simple__info-link" href="mailto:{!! variable('contact_email') !!}">
-                            {!! variable('contact_email') !!}</a>
-                    </div>
+                    @php($contact_email = variable('contact_email'))
+                    @if ($contact_email)
+                        <div class="contacts-simple__info-row" data-contacts="mail">
+                            <a class="contacts-simple__info-link" href="mailto:{!! $contact_email !!}">
+                                {!! $contact_email !!}</a>
+                        </div>
+                    @endif
 
-                    <div class="contacts-simple__info-row" data-contacts="skype">
-                        <a class="contacts-simple__info-link" href="skype:{!! variable('skype_name') !!}?call">
-                            {!! variable('skype_name') !!}
-                        </a>
-                    </div>
+                    @php($skype_name = variable('skype_name'))
+                    @if ($skype_name)
+                        <div class="contacts-simple__info-row" data-contacts="skype">
+                            <a class="contacts-simple__info-link" href="skype:{!! $skype_name !!}?call">
+                                {!! $skype_name !!}
+                            </a>
+                        </div>
+                    @endif
                 </li>
             </ul>
         </div>
