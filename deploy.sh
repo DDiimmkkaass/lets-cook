21cd6d9d34f54a381c794ea0971c3be3f5f14850
DDiimmkkaass <ddiimmkkaass@gmail.com>
2016-11-17 15:35:10 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #git reset --hard
 #git pull origin master
-composer install --no-interaction --prefer-dist
+#composer install --no-interaction --prefer-dist
 /opt/plesk/php/5.6/bin/php artisan migrate --force
 ./node_modules/.bin/bower install
 npm install
