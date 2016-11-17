f3591b0441faee14eacf1140987205a216ccf9ce
DDiimmkkaass <ddiimmkkaass@gmail.com>
2016-11-17 15:58:24 +0200

    check if social app configs set for oauth

--- resources/themes/lets_cook/views/auth/partials/social_login.blade.php
+++ resources/themes/lets_cook/views/auth/partials/social_login.blade.php
@@ -1,5 +1,11 @@
+@if (config('oauth-5-laravel.consumers.Vkontakte.client_id') || config('oauth-5-laravel.consumers.Facebook.client_id'))
 <div class="social-login">
-    <a class="social-login-item vkontakte" href="{!! localize_route('auth.social', 'vkontakte') !!}"></a>
+    @if (config('oauth-5-laravel.consumers.Vkontakte.client_id'))
+        <a class="social-login-item vkontakte" href="{!! localize_route('auth.social', 'vkontakte') !!}"></a>
+    @endif
 
-    <a class="social-login-item facebook" href="{!! localize_route('auth.social', 'facebook') !!}"></a>
+    @if (config('oauth-5-laravel.consumers.Facebook.client_id'))
+        <a class="social-login-item facebook" href="{!! localize_route('auth.social', 'facebook') !!}"></a>
+    @endif
 </div>
+@endif
