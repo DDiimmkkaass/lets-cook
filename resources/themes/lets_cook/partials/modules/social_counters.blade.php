168816c28675b6f3b38dc55c60299cd97ccdb21a
DDiimmkkaass <ddiimmkkaass@gmail.com>
2016-12-09 14:50:18 +0200

    fix social shares on footer

--- resources/themes/lets_cook/partials/modules/social_counters.blade.php
+++ resources/themes/lets_cook/partials/modules/social_counters.blade.php
@@ -1,6 +1,6 @@
 <div class="footer__section-2 social-info">
     <div class="social-info__list ya-share2"
-         data-services="facebook,vkontakte,gplus,lj"
+         data-services="facebook,vkontakte,gplus,odnoklassniki"
          data-lang="{!! $lang !!}"
          @unless (empty($home_page))
              data-title="{!! $home_page->getMetaTitle() !!}"
@@ -8,5 +8,5 @@
              data-image="{!! $home_page->getMetaImage() !!}"
              data-url="{!! $home_page->getUrl() !!}"
          @endunless
-         data-counter=""></div>
+    ></div>
 </div>
